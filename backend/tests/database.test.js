8821e8b080e6422891294b3fb3ad43d8bb8c0789
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2024-07-09 23:41:46 +0100

    test module created and database test sun succesfully

---
+++ backend/tests/database.test.js
@@ -0,0 +1,33 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv').config();
+
+
+describe('Database Connection', () => {
+  let sequelize;
+
+  beforeAll(() => {
+    sequelize = new Sequelize(
+      'test_database',
+      'test_user',
+      'Test_password1.',
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false
+      }
+    );
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  test('should establish a connection successfully', async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection established successfully!');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  });
+});
