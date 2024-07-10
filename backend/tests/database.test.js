b7a7c8da6e2cdc23dfff3ceb732f3b5493f38949
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2024-07-10 01:17:43 +0100

    module docs description added

--- backend/tests/database.test.js
+++ backend/tests/database.test.js
@@ -1,27 +1,36 @@
-const { Sequelize } = require("sequelize");
-const dotenv = require("dotenv").config();
+/**
+ * This file is responsible for testing the database functionality.
+ */
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv').config();
 
-describe("Database Connection", () => {
+
+describe('Database Connection', () => {
   let sequelize;
 
   beforeAll(() => {
-    sequelize = new Sequelize("test_database", "test_user", "Test_password1.", {
-      host: "localhost",
-      dialect: "mysql",
-      logging: false,
-    });
+    sequelize = new Sequelize(
+      'test_database',
+      'test_user',
+      'Test_password1.',
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false
+      }
+    );
   });
 
   afterAll(async () => {
     await sequelize.close();
   });
 
-  test("should establish a connection successfully", async () => {
+  test('should establish a connection successfully', async () => {
     try {
       await sequelize.authenticate();
-      console.log("Connection established successfully!");
+      console.log('Connection established successfully!');
     } catch (error) {
-      console.error("Unable to connect to the database:", error);
+      console.error('Unable to connect to the database:', error);
     }
   });
 });
