f35313ed1a9c30de8c1f97ce50a829cd9d7c44dd
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2024-07-18 22:42:22 +0100

    authentication route module created

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../services/auth.service');
+
+
+/**
+ * Represents the result of the login operation.
+ * @typedef {Object} LoginResult
+ * @property {boolean} success - Indicates whether the login was successful or not.
+ * @property {string} message - A message describing the result of the login operation.
+ * @property {string} token - The authentication token generated upon successful login.
+ */
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+	  const result = await login(email, password);
+	  res.status(result.status).json(result);
+	} catch (error) {
+	  console.error('Error logging in user:', error);
+	  res.status(500).json({ message: 'Internal server error' }); // Generic error for unexpected issues
+	}
+});
+
+
+
+/**
+ * Represents the result of the registration process.
+ * @typedef {Object} RegistrationResult
+ * @property {boolean} success - Indicates whether the registration was successful or not.
+ * @property {string} message - A message describing the result of the registration.
+ */
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await register(email, password);
+    res.status(result.status).json(result);
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+module.exports = router;
