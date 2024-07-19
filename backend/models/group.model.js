833fd94cdd04500e7f9b709b980c3ca9d6973f03
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-19 23:25:57 +0100

    Group object definition

--- backend/models/group.model.js
+++ backend/models/group.model.js
@@ -1,1 +1,21 @@
-const { se}
+const { storage } = require('../config/database')
+const { DataTypes } = require('sequelize')
+
+const Group = storage.define('Group', {
+  id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+});
+
+module.exports = Group
