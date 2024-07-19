da5253b4121299b28ba5343161e4fc62c72be08e
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-19 22:55:04 +0100

    comments and unique field

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,17 @@
 const { DataTypes, sequelize } = require("sequelize");
 const sequelize = require("../config/database");
 
+/**
+ * Represent the User model.
+ * @typedef {Object} User
+ * @property {string} id - represents user identity string.
+ * @property {string} username - represents user unique username string.
+ * @property {string} email - represents user email.
+ * @property {Date} createdAt - represents the date user is added to the system.
+ * @property {Date} updatedAt - represents the date user info is last updated.
+ */
+
+
 const User = sequelize.define("User", {
   id: {
     type: DataTypes.UUID,
@@ -11,10 +22,12 @@ const User = sequelize.define("User", {
   username: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   createdAt: {
     type: DataTypes.DATE,
