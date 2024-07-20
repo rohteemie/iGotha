a285c4ffb89d3f031256d97a8b237450d9fe1957
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-20 18:39:32 +0100

    message model

---
+++ backend/models/message.model.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Message = sequelize.define('Message', {
+  message_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  timestamp: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  read_receipt: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  typing_indicator: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Message;
