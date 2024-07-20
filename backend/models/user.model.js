7c1b508a8c4e52cb1b60712e4bec69211a5f2f1f
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-20 19:46:24 +0100

    comprehensive docstring

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,17 +1,21 @@
-const { DataTypes, sequelize } = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 const { storage } = require("../config/database");
 
 /**
- * Represent the User model.
+ * Represents User model definition
+ * 
  * @typedef {Object} User
- * @property {string} id - represents user identity string.
- * @property {string} username - represents user unique username string.
- * @property {string} email - represents user email.
- * @property {Date} createdAt - represents the date user is added to the system.
- * @property {Date} updatedAt - represents the date user info is last updated.
+ * @property {number} user_id - The unique identifier for the user.
+ * @property {string} username - The username of the user.
+ * @property {string} [email] - The email of the user (nullable for guest).
+ * @property {string} [password] - The password of the user (nullable for guest).
+ * @property {('online'|'offline')} status - The status of the user.
+ * @property {Date} [last_seen] - The last seen timestamp of the user.
+ * @property {Date} created_at - The creation date of the user record.
+ * @property {boolean} is_guest - Indicates if the user is a guest.
  */
 
-
+/** @type {Model} */
 const User = storage.define("User", {
   id: {
     type: DataTypes.UUID,
