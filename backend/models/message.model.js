96519a17095f51414ca3e2e845d818d7d9550086
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-20 21:11:52 +0100

    corrected import bug

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { storage } = require('../config/database');
 
 /**
  * Represents the Message model
