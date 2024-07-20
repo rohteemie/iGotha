45c47ca50b0f8679e2982977986247899b1ebda3
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-20 18:58:46 +0100

    define relationship between models

---
+++ backend/models/associations.model.js
@@ -0,0 +1,32 @@
+const { User } = require('./user.model')
+const { Message  } = require('./message.model')
+const { Chat } = require('./chat.model')
+const { Group } = require('./group.model')
+
+
+// A User can participate in multiple Chats
+User.belongsToMany(Chat, { through: 'UserChat' });
+Chat.belongsToMany(User, { through: 'UserChat' });
+
+// A User can be part of multiple Groups
+User.belongsToMany(Group, { through: 'UserGroup' });
+Group.belongsToMany(User, { through: 'UserGroup' });
+
+// A User can have multiple Messages
+User.hasMany(Message);
+Message.belongsTo(User);
+
+// A Message is part of one Chat
+Message.belongsTo(Chat);
+Chat.hasMany(Message);
+
+// A Message can optionally be part of a Group
+Message.belongsTo(Group);
+Group.hasMany(Message);
+
+module.exports = {
+  User,
+  Message,
+  Chat,
+  Group
+};
