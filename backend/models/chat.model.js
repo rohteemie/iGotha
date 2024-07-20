22dbd67cfb5c39e41c2bb3d3fb049608d9014c4b
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-20 18:31:59 +0100

    Chat object attrs

--- backend/models/chat.model.js
+++ backend/models/chat.model.js
@@ -1,6 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Chat = sequelize.define('Chat', {});
+const Chat = sequelize.define('Chat', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+    }
+});
 
 module.exports = Chat;
