2fb115c9b88c4694bf6c559bbe40f88f6287c827
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2024-08-24 09:50:18 +0100

    main application file updated with auth and user service registartion

--- backend/app.js
+++ backend/app.js
@@ -4,29 +4,27 @@ const http = require('http');
 const { storage } = require('./config/database');
 const handleSocketConn = require('./services/socketHandlers');
 require('./models/associations.model');
-const userRouter = require('./routes/user.router');
+const authRoutes = require('./routes/auth.route');
+const userRoutes = require('./routes/user.route');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-
 app.use(express.json());
-app.use('/users', userRouter);
+app.use('/users', userRoutes);
+app.use('/users', authRoutes);
 
-
-// Define a route
 app.get('/', (req, res) => {
   res.send('Hi, Welcome to iGotha chat app.');
 });
 
 handleSocketConn(io);
 
-// Start the server
 server.listen(PORT, async () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
   await storage.sync();
   console.log('Database created/synced!')
 });
