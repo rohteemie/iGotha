021f5f7b6148617af68d0e934b39d03a722be058
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-12-31 10:44:38 +0100

    handle "connection, connect, disconnect" events and also deliver offline messages

--- backend/websocket/socketHandler.js
+++ backend/websocket/socketHandler.js
@@ -1,21 +1,38 @@
-const { handlePrivateChat } = require('./chats/privateChat');
-const { handleGroupChat } = require('./chats/groupChat');
+const { handlePrivateChat } = require("./chats/privateChat");
+const { handleGroupChat } = require("./chats/groupChat");
+const { deliverOfflineMessage } = require("./operations/offlineUserMessage");
+const { verifyToken } = require("../helper/auth.util");
+const { redis_client } = require("../config/redis.config");
 
 function setupSocketHandlers(io) {
-    io.on('connection', (socket) => {
-        console.log('A user connected:', socket.id);
+  io.on("connection", (socket) => {
+      userID = verifyToken(socket.handshake.auth.token);
+      console.log("A user connected:", socket.id, userID);
 
-        // Setup private chat handlers
-        handlePrivateChat(io, socket);
+    deliverOfflineMessage(io, socket, userID);
 
-        // Setup group chat handlers
-        handleGroupChat(io, socket);
+    // Setup private chat handlers
+    handlePrivateChat(io, socket, userID);
 
-        // Handle disconnect
-        socket.on('disconnect', () => {
-            console.log('A user disconnected:', socket.id);
-        });
+    // Setup group chat handlers
+    handleGroupChat(io, socket);
+
+    // Handle connect
+    socket.on("connect", () => {
+      deliverOfflineMessage(io, socket, userID);
     });
+
+    // Handle disconnect
+    socket.on("disconnect", () => {
+      console.log(
+        "A user disconnected. here is the socket id and user id:",
+        socket.id,
+        userID
+      );
+      redis_client.del(`${userID}`);
+      console.log("deleted user");
+    });
+  });
 }
 
 module.exports = { setupSocketHandlers };
