2833bfaf83321db7df8f12f74b08ba1b6b0e1261
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-12-31 10:47:47 +0100

    Upgraded private chat: save messages for offline user and deliver for online users

--- backend/websocket/chats/privateChat.js
+++ backend/websocket/chats/privateChat.js
@@ -1,10 +1,35 @@
-let userSockets = {};
+const { redis_client } = require("../../config/redis.config");
 
-function handlePrivateChat(io, socket) {
-    socket.on('privateMessage', ({ recipientId, message }) => {
-        console.log(`Private message from ${socket.id} to ${recipientId}: ${message}`);
-        io.to(recipientId).emit('privateMessage', { sender: socket.id, message: `${message}, I have sent back the message` });
-    });
+async function handlePrivateChat(io, socket, userID) {
+  socket.on("privateMessage", async ({ recipientId, message }) => {
+    console.log(
+      `Private message from ${socket.id} to ${recipientId}: ${message}`
+    );
+
+    try {
+      const recipientSocketId = await redis_client.get(recipientId);
+
+      if (recipientSocketId) {
+        // Recipient is online; send message directly
+        console.log(`recipient socket ID: ${recipientSocketId}`);
+        io.to(recipientSocketId).emit("privateMessage", {
+          sender: userID,
+          message: `${message}`,
+        });
+      } else {
+        // Recipient is offline; save the message in Redis
+        const offlineMessage = JSON.stringify({
+          sender: userID,
+          message: message,
+          timestamp: Date.now(),
+        });
+        await redis_client.rPush(`messages:${recipientId}`, offlineMessage);
+        console.log(`Message saved for offline user ${recipientId}`);
+      }
+    } catch (err) {
+      console.error("Error handling private message:", err);
+    }
+  });
 }
 
 module.exports = { handlePrivateChat };
