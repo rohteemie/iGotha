16fc8cf9390e0eb51913b7bf95a3c18ae7133273
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-12-31 10:45:03 +0100

    user self chat event

--- backend/websocket/chats/personalChat.js
+++ backend/websocket/chats/personalChat.js
@@ -1,7 +1,7 @@
 function handlePrivateChat(io, socket) {
     socket.on('personalMessage', ({ recipientId, message }) => {
         console.log(`Personal message from ${socket.id} to ${recipientId}: ${message}`);
-        io.to(recipientId).emit('privateMessage', { sender: socket.id, message: `${message}, I have sent back the message` });
+        io.to(recipientId).emit('privateMessage', { sender: socket.id, message: `${message}` });
     });
 }
 
