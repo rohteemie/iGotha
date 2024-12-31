0f1b044b0f4f6886c5682079c016ab889f3a816f
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-12-31 10:42:51 +0100

    handle redis error and connections; ensure redis is ready when needed

--- backend/config/redis.config.js
+++ backend/config/redis.config.js
@@ -1,7 +1,29 @@
-const redis = require('redis');
-const redis_client = redis.createClient();
+const redis = require("redis");
+const redis_client = redis.createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
 
-redis_client.on('error', (err) => console.error('Redis error:', err));
+redis_client.on("connect", () => console.log("Connected to Redis."));
+redis_client.on("error", (err) => console.error("Redis error:", err));
 
+// Ensure Redis connection is open before attempting to use it
+async function connectRedis() {
+  if (!redis_client.isOpen) {
+    try {
+      await redis_client.connect();
+      console.log("Connected to Redis");
+    } catch (err) {
+      console.error("Failed to connect to Redis:", err);
+    }
+  }
+}
+
+connectRedis();
+
+process.on("SIGINT", async () => {
+  await redis_client.quit(); // Graceful shutdown
+  console.log("Redis connection closed.");
+  process.exit(0);
+});
 
 module.exports = { redis_client };
