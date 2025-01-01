f96cdb626b06d51b12022df7834403437aa526c3
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-01-02 00:55:07 +0100

    remove unnecessary comments

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const { Server, socketIO } = require('socket.io');
 const http = require('http');
 const { storage } = require('./config/database');
-// const handleSocketConn = require('./services/socketHandlers');
-// require('./models/associations.model');
 const { setupSocketHandlers } = require('./websocket/socketHandler');
 const app = require('./app');
 const websocketAuthenticateTokenMiddleware = require('./middleware/auth.socket.middleware');
